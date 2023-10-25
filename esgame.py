3c90023bf3aeaf6e9cc6ed5a67ae025f9ffe6abf
eag-ehsan <eag.ehsan@gmail.com>
2023-10-25 11:37:21 +0330

    12

--- esgame.py
+++ esgame.py
@@ -1,1 +1,21 @@
 import pygame
+
+pygame.init();
+
+screen = pygame.display.set_mode((640,480))
+
+clock = pygame.time.Clock()
+
+while True:
+    
+    for event in pygame.event.get():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.update()
+    clock.tick(60)
+
+
